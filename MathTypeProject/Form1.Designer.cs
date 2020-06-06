e7354762e30e7dcfff21463a617c8a76d4d75c14
Piotr Walat <pwalat98@gmail.com>
2020-06-06 18:14:28 +0200

    Gui progress bar and visibility on/off feature added
    Progress bar linked to amount of OMath objects.
    Visibility as check box under file path.

--- MathTypeProject/Form1.Designer.cs
+++ MathTypeProject/Form1.Designer.cs
@@ -32,14 +32,20 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button1.Location = new System.Drawing.Point(359, 0);
+            this.button1.Location = new System.Drawing.Point(13, 47);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 161);
+            this.button1.Size = new System.Drawing.Size(100, 20);
             this.button1.TabIndex = 0;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,7 +55,7 @@
             // 
             this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
-            this.textBox1.Location = new System.Drawing.Point(12, 69);
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(247, 20);
             this.textBox1.TabIndex = 1;
@@ -57,8 +63,9 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 92);
+            this.label1.Location = new System.Drawing.Point(9, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(157, 13);
             this.label1.TabIndex = 2;
@@ -68,7 +75,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(265, 69);
+            this.button2.Location = new System.Drawing.Point(265, 34);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 20);
             this.button2.TabIndex = 3;
@@ -76,19 +83,84 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.BrowseButton_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 60);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(117, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Application Visibility";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Application is using clipboard to change math equation, do not usy copy feature w" +
+    "hile converting";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(15, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "WARNING!";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 18);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(310, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Progress:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(297, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(126, 70);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Progress:";
+            // 
             // EquationToLaTeXConverter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(434, 161);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.MaximumSize = new System.Drawing.Size(450, 200);
             this.MinimumSize = new System.Drawing.Size(450, 200);
             this.Name = "EquationToLaTeXConverter";
             this.Text = "EquationToLaTeXConverter";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,5 +172,11 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        public System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- MathTypeProject/WordDocumentParser.cs
+++ MathTypeProject/WordDocumentParser.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Windows.Forms;
 using System.Threading;
 
+
 namespace MathTypeProject
 {
     internal class WordDocumentParser : OfficeDocumentParser
@@ -18,10 +19,13 @@ namespace MathTypeProject
         Microsoft.Office.Interop.Word.Document docOpen;
         public List<Object> mathTypeEquations = new List<Object>();
         Word.Range myRange;
+        EquationToLaTeXConverter form;
+        bool visible = false;
 
 
-        public WordDocumentParser(string inputFilePath)
+        public WordDocumentParser(string inputFilePath, EquationToLaTeXConverter form)
         {
+            this.form = form;
             char[] separator = { '\\' };
             string[] directories = inputFilePath.Split(separator, StringSplitOptions.RemoveEmptyEntries);
             string dir = "";
@@ -33,13 +37,31 @@ namespace MathTypeProject
             this.inputFileName = directories[directories.Length - 1];
             Console.WriteLine(inputFileName);
             this.app = new Word.Application();
-            this.docOpen = app.Documents.Open(inputFilePath);
+            
+
+            if (form.checkBox1.Checked == true)
+            {
+                this.app.Visible = true;
+                Console.WriteLine("pokazuje");
+                this.visible = true;
+
+            }
+            if (form.checkBox1.Checked == false)
+            {
+                this.app.Visible = false;
+                //this.app.DisplayAlerts = 0;
+                Console.WriteLine("nie pokazuje");
+                this.visible = false;
+            }
+            this.docOpen = app.Documents.Open(inputFilePath, Visible: visible);
+
+           
+
             this.myRange = docOpen.Range();
 
             object isVisible = true;
             File.SetAttributes(inputFilePath, FileAttributes.Normal);
 
-            docOpen = this.app.Documents.Open(inputFilePath, Visible: isVisible);
             docOpen.Activate();
         }
 
@@ -50,25 +72,31 @@ namespace MathTypeProject
             {
                 Console.WriteLine(shape.TextFrame);
             }
-            Thread staThread = new Thread(
-            delegate ()
+
+            int OMathsCount = myRange.OMaths.Count;
+            Console.WriteLine(OMathsCount);
+            Console.WriteLine("Bar max:");
+
+            form.progressBar1.Maximum = OMathsCount;
+
+
+            try
             {
-                try
+                String clipboard_memory = Clipboard.GetText();
+                Word.Range endRange = docOpen.Range(myRange.End - 1, myRange.End - 1);
+
+
+
+                if (OMathsCount > 0)
                 {
-                    String clipboard_memory = Clipboard.GetText();
-                    Word.Range endRange = docOpen.Range(myRange.End - 1, myRange.End - 1);
-
-                    int OMathsCount = myRange.OMaths.Count;
-                    Console.WriteLine(OMathsCount);
-
-                    if (OMathsCount > 0)
+                    string temp_file_path = this.inputFileDir + @"\EquationTemporaryFile.txt";
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(temp_file_path))
                     {
-                        string temp_file_path = this.inputFileDir + @"\EquationTemporaryFile.txt";
-                        using (System.IO.StreamWriter file = new System.IO.StreamWriter(temp_file_path))
+                        for(int i=0; i<OMathsCount; i++)
                         {
-                            while (myRange.OMaths != null)
+                            Thread staThread = new Thread(
+                            delegate ()
                             {
-                                //myRange.OMaths[i].ConvertToNormalText();
                                 Word.OMath currentEquation = myRange.OMaths[1];
 
                                 currentEquation.Range.Select();
@@ -76,24 +104,27 @@ namespace MathTypeProject
                                 currentEquation.Range.TextRetrievalMode.IncludeHiddenText = true;
                                 currentEquation.Range.TextRetrievalMode.IncludeFieldCodes = true;
 
-                                // @Diagnostic
                                 currentEquation.Range.Application.Selection.Range.HighlightColorIndex = Word.WdColorIndex.wdYellow;
 
                                 currentEquation.Range.Application.Selection.Copy();
 
 
                                 String tekst = Clipboard.GetText();
+                                String new_tekst = "$$";
                                 /////////////////////////////////////////////////////////////////////////////////////
 
                                 char[] tokens = tekst.ToCharArray();
-                                string[] parsed = parseTokensToTex(tokens);
-                                foreach(string p in parsed)
+                                string[] parsed = translateTokensToTex(tokens);
+                                for (int p = 0; p < parsed.Length; p++)
                                 {
-                                    Console.WriteLine(p);
+                                    Console.Write(parsed[p] + "^.^");
                                 }
-                                Console.WriteLine("koniec");
-
-
+                                Console.WriteLine("---------------------------------------------------");
+                                for (int p = 0; p < parsed.Length; p++)
+                                {
+                                    new_tekst += parseToken(ref parsed, p);
+                                }
+                                new_tekst += "$$";
                                 /////////////////////////////////////////////////////////////////////////////////////
                                 file.WriteLine(tekst);
                                 if (clipboard_memory.CompareTo("") != 0)
@@ -107,33 +138,37 @@ namespace MathTypeProject
 
                                 //removing text from start to end
 
-                                
                                 int start = currentEquation.Range.Start;
                                 int end = currentEquation.Range.End;
                                 currentEquation.Range.Application.Selection.Delete();
-                                
-                                
-                            }
+                                currentEquation.Range.InsertBefore(new_tekst);
+                            });
+            
+                            staThread.SetApartmentState(ApartmentState.STA);
+                            staThread.Start();
+                            staThread.Join();
+                            form.progressBar1.PerformStep();
                         }
                     }
-                    else
-                    {
-                        MessageBox.Show("No equations found.");
-                    }
-
-                    MessageBox.Show("Process Completed");
                 }
-                catch (Exception)
+                else
                 {
-                    throw;
+                    MessageBox.Show("No equations found.");
                 }
-            });
-            staThread.SetApartmentState(ApartmentState.STA);
-            staThread.Start();
-            staThread.Join();
+                docOpen.Close();
+                app.Quit();
+                MessageBox.Show("Process Completed");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
 
         }
-        private string[] parseTokensToTex(char[] tokens)
+
+
+        private string[] translateTokensToTex(char[] tokens)
         {
             int iterator = 0;
             string[] new_tokens = new string[tokens.Length];
@@ -152,6 +187,208 @@ namespace MathTypeProject
                 iterator++;
             }
             return new_tokens;
+        }
+
+        private string parseSqrt(ref string[] parsed, int p)
+        {
+            if (parsed[p + 1] == "(")
+            {
+                int amp_idx = 0;
+                int par_count = 1;
+                int temp_idx = p + 2;
+                while (par_count != 0)
+                {
+                    if (parsed[temp_idx] == "(")
+                    {
+                        par_count++;
+                    }
+                    else if (parsed[temp_idx] == ")")
+                    {
+                        par_count--;
+                    }
+                    else if (parsed[temp_idx] == "&")
+                    {
+                        amp_idx = temp_idx;
+                        temp_idx = p + 2;
+                        parsed[p] = @"\sqrt[";
+                        parsed[p + 1] = @"";
+                        while (temp_idx != amp_idx)
+                        {
+                            parsed[p] += parsed[temp_idx];
+                            parsed[temp_idx] = @"";
+                            temp_idx++;
+                        }
+                        parsed[temp_idx] = @"";
+                        parsed[p] += @"]{}";
+                    }
+                    else
+                    {
+                        parsed[temp_idx] = parseToken(ref parsed, temp_idx);
+                    }
+                    temp_idx++;
+                }
+                parsed[p + 1] = @"";
+                parsed[temp_idx - 1] = @"";
+                parsed[p] = parsed[p].Substring(0, parsed[p].Length - 1);
+                if(amp_idx == 0)
+                {
+                    amp_idx = p + 1;
+                }
+                for(int idx = amp_idx+1; idx < temp_idx-1; idx++)
+                {
+                    parsed[p] += parsed[idx];
+                    parsed[idx] = @"";
+                }
+                parsed[p] += @"}";
+            }
+            else
+            {
+                int temp_idx = p + 1;
+                while(temp_idx < parsed.Length && Char.IsDigit(parsed[temp_idx].ToCharArray()[0]))
+                {
+                    temp_idx++;
+                }
+                parsed[p] = parsed[p].Substring(0, parsed[p].Length - 1);
+                for (int idx = p + 1; idx < temp_idx; idx++)
+                {
+                    parsed[p] += parsed[idx];
+                    parsed[idx] = @"";
+                }
+                parsed[p] += @"}";
+            }
+            return parsed[p];
+        }
+
+        private string parseGenericTokenWithCurlyBraces(ref string[] parsed, int p)
+        {
+            if (parsed[p + 1] == "(")
+            {
+                int par_count = 1;
+                int temp_idx = p + 2;
+                while (par_count != 0)
+                {
+                    if (parsed[temp_idx] == "(")
+                    {
+                        par_count++;
+                    }
+                    else if (parsed[temp_idx] == ")")
+                    {
+                        par_count--;
+                    }
+                    else
+                    {
+                        parsed[temp_idx] = parseToken(ref parsed, temp_idx);
+                    }
+                    temp_idx++;
+                }
+                parsed[p + 1] = @"";
+                parsed[temp_idx - 1] = @"";
+                parsed[p] = parsed[p].Substring(0, parsed[p].Length - 1);
+                for (int idx = p + 2; idx < temp_idx - 1; idx++)
+                {
+                    parsed[p] += parsed[idx];
+                    parsed[idx] = @"";
+                }
+                parsed[p] += @"}";
+            }
+            else
+            {
+                int temp_idx = p + 1;
+                while (temp_idx < parsed.Length && Char.IsDigit(parsed[temp_idx].ToCharArray()[0]))
+                {
+                    temp_idx++;
+                }
+                parsed[p] = parsed[p].Substring(0, parsed[p].Length - 1);
+                for (int idx = p + 1; idx < temp_idx; idx++)
+                {
+                    parsed[p] += parsed[idx];
+                    parsed[idx] = @"";
+                }
+                parsed[p] += @"}";
+            }
+            return parsed[p];
+        }
+
+        private string parseSubSup(ref string[] parsed, int p)
+        {
+            if (parsed[p + 1] == "(")
+            {
+                int par_count = 1;
+                int temp_idx = p + 2;
+                while (par_count != 0)
+                {
+                    if (parsed[temp_idx] == "(")
+                    {
+                        par_count++;
+                    }
+                    else if (parsed[temp_idx] == ")")
+                    {
+                        par_count--;
+                    }
+                    else
+                    {
+                        parsed[temp_idx] = parseToken(ref parsed, temp_idx);
+                    }
+                    temp_idx++;
+                }
+                parsed[p + 1] = @"";
+                parsed[temp_idx - 1] = @"";
+                parsed[p] = parsed[p].Substring(0, parsed[p].Length - 1);
+                for (int idx = p + 2; idx < temp_idx - 1; idx++)
+                {
+                    parsed[p] += parsed[idx];
+                    parsed[idx] = @"";
+                }
+                parsed[p] += @"}";
+                if (temp_idx != parsed.Length && parsed[temp_idx] == "big operator separator")
+                {
+                    parsed[temp_idx] = @"";
+                }
+            }
+            else
+            {
+                int temp_idx = p + 1;
+                while (temp_idx < parsed.Length && parsed[temp_idx] != @"^{}" && parsed[temp_idx] != "big operator separator")
+                {
+                    temp_idx++;
+                }
+                if (temp_idx != parsed.Length && parsed[temp_idx] == "big operator separator")
+                {
+                    parsed[temp_idx] = @"";
+                }
+                parsed[p] = parsed[p].Substring(0, parsed[p].Length - 1);
+                for (int idx = p + 1; idx < temp_idx; idx++)
+                {
+                    parsed[p] += parsed[idx];
+                    parsed[idx] = @"";
+                }
+                parsed[p] += @"}";
+            }
+            return parsed[p];
+        }
+
+        private string parseToken(ref string[] parsed, int index)
+        {
+            if (parsed[index] == @"\sqrt[]{}")
+            {
+                return parseSqrt(ref parsed, index);
+            }
+            else if (parsed[index] == @"\cbrt{}" || parsed[index] == @"\qdrt{}")
+            {
+                return parseGenericTokenWithCurlyBraces(ref parsed, index);
+            }
+            else if (parsed[index] == @"^{}" || parsed[index] == @"_{}")
+            {
+                return parseSubSup(ref parsed, index);
+            }
+            else if (parsed[index] == "big operator separator")
+            {
+                return @"";
+            }
+            else
+            {
+                return parsed[index];
+            }
         }
 
         public void parse() {}
